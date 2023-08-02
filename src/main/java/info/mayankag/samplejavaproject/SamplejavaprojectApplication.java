fef9194fe2f67ea99d1723b16a32f8e7c7e10c96
Mayank <mayankag.professional@proton.me>
2023-08-02 17:45:30 +0200

    Initial Commit
    This is the first commit for this application

---
+++ src/main/java/info/mayankag/samplejavaproject/SamplejavaprojectApplication.java
@@ -0,0 +1,13 @@
+package info.mayankag.samplejavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplejavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplejavaprojectApplication.class, args);
+	}
+
+}
